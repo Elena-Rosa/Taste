055febc04c11823b9b012175966d85b07d1661b8
Elena <elenarosamail@gmail.com>
2023-11-12 09:41:10 -0800

    Project Complete

--- Taste/Program.cs
+++ Taste/Program.cs
@@ -9,35 +9,51 @@ namespace Taste
   class Program
   {
     static void Main(string[] args)
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllersWithViews();
-            // be sure to update the line below for your project
-            builder.Services.AddDbContext<TasteContext>(options => options
-            .UseMySql(
-            builder.Configuration["ConnectionStrings:DefaultConnection"],
-            ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
-    )
-);      
-            // Line below adds Identity
-            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-                      .AddEntityFrameworkStores<TasteContext>()
-                      .AddDefaultTokenProviders();      
-             
-            WebApplication app = builder.Build();       
-            app.UseDeveloperExceptionPage();
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();       
-            app.UseRouting();       
-            // Next two lines below enable authentication and authorization.
-            app.UseAuthentication();
-            app.UseAuthorization();     
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}"
-              );        
-            app.Run();
+      builder.Services.AddControllersWithViews();
+      // be sure to update the line below for your project
+      builder.Services.AddDbContext<TasteContext>(options => options
+      .UseMySql(
+      builder.Configuration["ConnectionStrings:DefaultConnection"],
+      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+)
+);
+      // Line below adds Identity
+      builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<TasteContext>()
+                .AddDefaultTokenProviders();
+
+      builder.Services.Configure<IdentityOptions>(options =>
+{
+  // options.Password.RequireDigit = true;
+  // options.Password.RequireLowercase = true;
+  // options.Password.RequireNonAlphanumeric = true;
+  // options.Password.RequireUppercase = true;
+  // options.Password.RequiredLength = 6;
+  // options.Password.RequiredUniqueChars = 1;
+  options.Password.RequireDigit = false;
+  options.Password.RequireLowercase = false;
+  options.Password.RequireNonAlphanumeric = false;
+  options.Password.RequireUppercase = false;
+  options.Password.RequiredLength = 0;
+  options.Password.RequiredUniqueChars = 0;
+});
+
+      WebApplication app = builder.Build();
+      app.UseDeveloperExceptionPage();
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseRouting();
+      // Next two lines below enable authentication and authorization.
+      app.UseAuthentication();
+      app.UseAuthorization();
+      app.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+      app.Run();
     }
   }
 }
